563e081dd25c1d6933032fa571c8ed716d35871f
Ryan York <awryanyork@gmail.com>
2017-04-05 12:54:04 -0500

    finished input practice project

---
+++ JavaScript Practice/Input to string practice/script.js
@@ -0,0 +1,7 @@
+// take in someones name, age, and reddit username and put it into a string on the page
+
+var name = prompt('What is your first name?');
+var age = prompt('How old are you?');
+var reddit = prompt('What is your reddit username?');
+
+document.write( 'Your name is ' + name + ', you are ' + age + ' years old, and your reddit username is ' + reddit + '.' );
