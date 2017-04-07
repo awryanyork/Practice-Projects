35e9ecdec92aeb2b173c455f41947f170f87d881
Ryan York <awryanyork@gmail.com>
2017-04-07 13:28:56 -0500

    finished an awful mad lib game

---
+++ JavaScript Practice/mad lib/script.js
@@ -0,0 +1,10 @@
+alert('Welcome to the Mad Lib game, please input your responses into the prompts and then await the reveal of your wacky mad lib');
+
+var noun1 = prompt('Please enter a noun');
+var adjective1 = prompt('Please enter an adjective');
+var verb1 = prompt('Please enter a verb');
+var noun2 = prompt('Please enter a different noun');
+var adjective2 = prompt('Please enter a different adjective');
+var verb2 = prompt('Please enter a different verb');
+
+document.write('Once upon a time there was a ' + noun1 + ' that would always ' + verb1 + ' and this made everyone very ' + adjective1 + ', so finally ' + noun2 + ' ' + verb2 + ' which made the mayor very ' + adjective2 + '.');
